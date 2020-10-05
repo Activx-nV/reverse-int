c27cc2a272af1ec9cf9ab20e2668b80e4252febd
Lauri K <laurikamm@gmail.com>
2020-10-05 15:40:22 +0300

    feat: make basic functional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        let MinusToPlusDigit = String(n).split('').reverse();
+        let minusHolder = MinusToPlusDigit.pop();
+        return MinusToPlusDigit.join('');
+    } else return String(n).split('').reverse().join('');
+
 }
